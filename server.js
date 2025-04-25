fa4917c9f180a7125496e1756d9cca0899daa7d3
Tushar <shtushar1310@gmail.com>
2025-04-25 15:31:20 +0530

    sixth changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose=require('mongoose')
+const cors = require("cors");
 require('dotenv').config();
 app.use(cors());
 
